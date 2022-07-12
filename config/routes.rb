0d569f6e93a686fc6da07546ec78716c087d3d81
Aryan Pal <aryanpalongraph@gmail.com>
2022-07-12 12:34:47 +0530

    ✅ Added Edit Features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+  get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new"
+  get "/articles/:id/edit", to: "articles#edit"
+  patch "/articles/:id", to: "articles#update"
+  get "/articles/:id", to: "articles#show"
+  
+  # resources :articles 
 end
